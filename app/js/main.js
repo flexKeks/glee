2363ab0fdf709820949059ad4084d2ebac17db97
flexKeks <getRight433Simple@gmail.com>
2020-11-20 19:12:37 +0300

    added slier and trends section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
-$(function () {});
+$(function () {
+  $(".top-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  var mixer = mixitup(".trends__items");
+});
