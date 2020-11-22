fd5a0b3e4e5f5782eb70e41a374100b747107aee
flexKeks <getRight433Simple@gmail.com>
2020-11-22 22:42:47 +0300

    added new products section, video section, blog section

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,9 @@ $(function () {
     dots: true,
     arrows: false,
     fade: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
   });
   var mixer = mixitup(".trends__items");
+  var secondMixer = mixitup(".new-products__items");
 });
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function browsersync() {
 }
 
 function styles() {
-  return src("app/scss/style.scss")
+  return src("app/scss/index/style.scss")
     .pipe(scss({ outputStyle: "compressed" }))
     .pipe(concat("style.min.css"))
     .pipe(
@@ -50,6 +50,7 @@ function scripts() {
     "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
     "node_modules/mixitup/dist/mixitup.js",
+    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
     "app/js/main.js",
   ])
     .pipe(concat("main.min.js"))
