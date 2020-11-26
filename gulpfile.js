dc6f4d82018049b11c41bcbe90f3b3fb7b80e8f3
flexKeks <getRight433Simple@gmail.com>
2020-11-26 18:13:02 +0300

    fixed errors

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function browsersync() {
 }
 
 function styles() {
-  return src("app/scss/index/style.scss")
+  return src("app/scss/style.scss")
     .pipe(scss({ outputStyle: "compressed" }))
     .pipe(concat("style.min.css"))
     .pipe(
