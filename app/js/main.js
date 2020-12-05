203d58ab98baf9f6d41fb8bc021e9799e24b915f
flexKeks <getRight433Simple@gmail.com>
2020-12-05 20:39:04 +0300

    added product details page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,33 @@
 $(function () {
+  $(".product-tabs__top-item").on("click", function (e) {
+    e.preventDefault();
+    $(".product-tabs__top-item").removeClass("product-tabs__top-item--active");
+    $(this).addClass("product-tabs__top-item--active");
+
+    $(".product-tabs__content-item").removeClass(
+      "product-tabs__content-item--active"
+    );
+    $($(this).attr("href")).addClass("product-tabs__content-item--active");
+  });
+
+  $(".product-details__form-input").styler();
+
+  $(".product-slide__thumb").slick({
+    asNavFor: ".product-slide__big",
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+
+  $(".product-slide__big").slick({
+    asNavFor: ".product-slide__thumb",
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
   $(".star").rateYo({
     rating: 3.6,
     starWidth: "11px",
@@ -7,6 +36,7 @@ $(function () {
     readOnly: true,
     spacing: "5px",
   });
+
   $(".star--big").rateYo({
     rating: 3.6,
     starWidth: "18px",
@@ -15,6 +45,7 @@ $(function () {
     readOnly: true,
     spacing: "5px",
   });
+
   $(".filter-price__input").ionRangeSlider({
     type: "double",
     prefix: "$",
@@ -35,6 +66,7 @@ $(function () {
     arrows: false,
     fade: true,
   });
+
   var mixer = mixitup(".trends__items");
   var secondMixer = mixitup(".new-products__items");
 });
